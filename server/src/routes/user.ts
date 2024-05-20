fb0b2a7ef35c857662b052ab4361eae899dd4d35
JustMarcus123 <141229878+JustMarcus123@users.noreply.github.com>
2024-05-20 15:06:11 +0530

    backend fist wave (20-may-2024)

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import { IUser, UserModel } from '../models/user';
 import { UserErrors } from '../Error';
 import bcrypt from 'bcrypt';
@@ -33,10 +33,10 @@ router.post('/login', async(req:Request, res:Response)=>{
 
     try {
 
-        const user:IUser = await UserModel.findOne({username,});
+        const user:IUser = await UserModel.findOne({username});
 
         if(!username){
-            return res.status(404).json({type:UserErrors.NO_USER_FOUND})
+            return res.status(400).json({type:UserErrors.NO_USER_FOUND})
         }
 
         const isPasswordValid = await bcrypt.compare(password, user.password);
@@ -54,5 +54,19 @@ router.post('/login', async(req:Request, res:Response)=>{
 
     }
 });
+
+//jwt authentication
+export const verifyToken =(req:Request, res:Response, next:NextFunction)=>{
+    const authHeader = req.headers.authentication;
+    if(authHeader){
+        jwt.verify(authHeader,"secret",(err)=>{
+            if(err){
+                return res.sendStatus(403);
+            }
+
+            next();
+        });
+    }
+}
 
 export { router as userRouter };
